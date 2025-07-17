4e84f73386f070e982db308c2374ca5863292f20
Frank-NikoAI <frank@4property.com>
2025-07-17 16:50:39 +0100

    Deleted README.md

--- Elasticsearch-to-MySQL-master/Elasticsearch-to-MySQL-master/PMX-api/app/api/core/config/settings.py
+++
@@ -1,5 +0,0 @@
-class TestingConfig:
-    TESTING = True
-    PMX_DATABASE_URI = "mysql+pymysql://root@localhost:3306/pmx_report"
-    API_KEY_DATABASE_URI = "mysql+pymysql://root@localhost:3306/pmx_report"
-    URL = "http://127.0.0.1:8000/"
